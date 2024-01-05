8ffb9d696ef06307e7dc0e7b5773af08d707c8cd
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-05 12:05:17 +0900

    ..

---
+++ src/main/generated/org/choongang/member/entities/QMember.java
@@ -0,0 +1,56 @@
+package org.choongang.member.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -1096527217L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final org.choongang.commons.entities.QBase _super = new org.choongang.commons.entities.QBase(this);
+
+    public final ListPath<Authorities, QAuthorities> authorities = this.<Authorities, QAuthorities>createList("authorities", Authorities.class, QAuthorities.class, PathInits.DIRECT2);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath email = createString("email");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public final StringPath userId = createString("userId");
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
