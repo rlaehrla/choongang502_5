b924156c8831ae8fed144b1fa49c8297ae8e23bb
yunii118 <lyhee118@gmail.com>
2024-01-08 19:43:11 +0900

    blog ajax 반쯤적용

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -6,9 +6,13 @@ import org.choongang.board.repositories.BoardDataRepository;
 import org.choongang.commons.Utils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +22,17 @@ public class BoardController {
     @GetMapping("/")
     public String main(){
         return utils.tpl("board/index");
+    }
+
+    @GetMapping("/farm/blog")
+    public String blog(Model model){
+        List<String> addScript = new ArrayList<>();
+        List<String> addCommonScript = new ArrayList<>();
+        addScript.add("board/blog");
+        addCommonScript.add("common");
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("addCommonScript", addCommonScript);
+        return utils.tpl("blog/farmerblog");
     }
 
 }
