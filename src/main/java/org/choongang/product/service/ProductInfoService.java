d759a997db95fa9d3b032166322a900a1567bb8b
yhg1024 <dgr1024kr@naver.com>
2024-01-09 20:05:31 +0900

    상품 목록 컨트롤러

---
+++ src/main/java/org/choongang/product/service/ProductInfoService.java
@@ -0,0 +1,10 @@
+package org.choongang.product.service;
+
+public class ProductInfoService {
+    /**
+     * public ProductData get(Long seq) {}
+     */
+    /**
+     * public ListData<ProductData> getList(....) {..}
+     */
+}
