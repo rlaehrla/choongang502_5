998ca12564a389d4085a0eadd503b92b2e7b1d27
yunii118 <lyhee118@gmail.com>
2024-01-10 17:52:44 +0900

    판매자 페이지 aside와 링크 연결

--- src/main/java/org/choongang/farmer/order/controller/OrderController.java
+++ src/main/java/org/choongang/farmer/order/controller/OrderController.java
@@ -1,9 +1,44 @@
 package org.choongang.farmer.order.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.MenuDetail;
+import org.choongang.commons.Utils;
+import org.choongang.farmer.menus.FarmerMenu;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller("farmerOrderController")
 @RequestMapping("/farm/manage/order")
+@RequiredArgsConstructor
 public class OrderController {
+    @ModelAttribute("menuCode")
+    public String getMenuCode(){
+        return "order";
+    }
+
+    @ModelAttribute("subMenus")
+    public List<MenuDetail> getSubMenus(){
+        return FarmerMenu.getMenus("order");
+    }
+
+    @GetMapping
+    public String order(Model model){
+        commonProcess("order", model);
+        return "admin/farmer/order/order";
+    }
+
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "order";
+        String pageTitle = "주문관리";
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("subMenuCode", mode);
+    }
 }
--- src/main/java/org/choongang/farmer/sales/controller/SalesController.java
+++ src/main/java/org/choongang/farmer/sales/controller/SalesController.java
@@ -1,10 +1,49 @@
 package org.choongang.farmer.sales.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.commons.MenuDetail;
+import org.choongang.farmer.menus.FarmerMenu;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller("farmerSalesController")
 @RequestMapping("/farm/manage/sales")
 public class SalesController {
+
+    @ModelAttribute("menuCode")
+    public String getMenuCode(){
+        return "sales";
+    }
+
+    @ModelAttribute("subMenus")
+    public List<MenuDetail> getSubMenus(){
+        return FarmerMenu.getMenus("sales");
+    }
+
+    @GetMapping
+    public String status(Model model){
+        // 매출 현황
+        commonProcess("sales", model);
+        return "admin/farmer/sales/sales_status";
+    }
+    @GetMapping("/product")
+    public String product(Model model){
+        // 상품관리
+        commonProcess("product", model);
+        return "admin/farmer/sales/product";
+    }
+
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "order";
+        String pageTitle = "주문관리";
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("subMenuCode", mode);
+    }
 }
