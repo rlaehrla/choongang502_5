748c29aa19545a6e0d402d918167f12c675c6342
ddoonsim <2sujg97@naver.com>
2024-01-10 18:27:36 +0900

    merge 충돌 수정

--- src/main/java/org/choongang/member/service/MemberInfoService.java
+++ src/main/java/org/choongang/member/service/MemberInfoService.java
@@ -1,10 +1,23 @@
 package org.choongang.member.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.choongang.commons.ListData;
+import org.choongang.commons.Pagination;
+import org.choongang.commons.Utils;
 import org.choongang.file.entities.FileInfo;
 import org.choongang.file.service.FileInfoService;
+import org.choongang.member.controllers.MemberSearch;
 import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.AbstractMember;
+import org.choongang.member.entities.Member;
+import org.choongang.member.entities.QMember;
 import org.choongang.member.repositories.MemberRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +33,8 @@ public class MemberInfoService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final FileInfoService fileInfoService;
+    private final EntityManager em ;
+    private final HttpServletRequest request ;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -51,5 +66,39 @@ public class MemberInfoService implements UserDetailsService {
                 .member(member)
                 .authorities(authorities)
                 .build();
+    }
+
+
+    /**
+     * 회원 목록 반환
+     */
+    public ListData<Member> getList(MemberSearch search) {
+
+        int page = Utils.onlyPositiveNumber(search.getPage(), 1) ;    // 현재 페이지 번호
+        int limit = Utils.onlyPositiveNumber(search.getLimit(), 20) ;    // 한 페이지 당 노출할 레코드 개수
+        int offset = (page - 1) * limit ;    // 레코드 시작 위치 번호
+
+        BooleanBuilder andBuilder = new BooleanBuilder() ;
+        QMember member = QMember.member ;
+
+        PathBuilder<Member> pathBuilder = new PathBuilder<>(Member.class, "member") ;
+
+        List<Member> items = new JPAQueryFactory(em)
+                .selectFrom(member)
+                .leftJoin(member.authorities)
+                .fetchJoin()
+                .where(andBuilder)    // 조건식
+                .limit(limit)
+                .offset(offset)
+                .orderBy(new OrderSpecifier(Order.DESC, pathBuilder.get("createdAt")))    // 정렬
+                .fetch() ;
+
+        /* 페이징 처리 S */
+        int total = (int) memberRepository.count(andBuilder) ;    // 총 레코드 개수
+        total = 123456 ;
+        Pagination pagination = new Pagination(page, total, 10, limit, request) ;
+        /* 페이징 처리 E */
+
+        return new ListData<>(items, pagination) ;
     }
 }
