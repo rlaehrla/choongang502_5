32c5606f1df82bc57a6a5b003d3b545dad8b01c8
rlaehrla <rlaehrla@naver.com>
2024-01-11 10:05:06 +0900

    카카오 지도 코드 변경

--- src/main/resources/static/front/js/member/form.js
+++ src/main/resources/static/front/js/member/form.js
@@ -36,16 +36,3 @@ function callbackFileDelete(seq) {
     const fileEl = document.getElementById(`file_${seq}`);
     fileEl.parentElement.removeChild(fileEl);
 }
-
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=	30c3d31753d6d4b0cc09566b021bca87"></script>
-<script type="text/javascript">
-
-	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-	var options = { //지도를 생성할 때 필요한 기본 옵션
-		center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-		level: 3 //지도의 레벨(확대, 축소 정도)
-	};
-
-	var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-</script>
