a5690bfb155abbceb8f4933922b6d5ea9b52606b
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-12 14:48:36 +0900

    240112 오후 상품 파트

--- src/main/java/org/choongang/product/entities/Category.java
+++ src/main/java/org/choongang/product/entities/Category.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.choongang.commons.entities.Base;
 import org.choongang.commons.entities.BaseMember;
+import org.choongang.member.entities.Farmer;
 import org.choongang.product.constants.MainCategory;
 
 @Entity
@@ -14,10 +16,14 @@ import org.choongang.product.constants.MainCategory;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(indexes = @Index(name = "idx_category_listOrder", columnList = "listOrder DESC, createdAt DESC"))
-public class Category extends BaseMember {
+public class Category extends Base {
     @Id
     @Column(length = 30)
-    private String cateCd; // 분류코드
+    private String cateCd; // 분류코드 -> 회원아이디_분류코드
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farmer_seq")
+    private Farmer farmer;
 
     @Enumerated(EnumType.STRING)
     @Column(length = 30, nullable = false)
