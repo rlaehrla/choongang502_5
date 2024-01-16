8bf5cfcef9434b4345e7c73949de8069795146bb
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-16 14:13:44 +0900

    회원 목록 - 회원 검색 기능 추가

--- src/main/java/org/choongang/member/controllers/MemberSearch.java
+++ src/main/java/org/choongang/member/controllers/MemberSearch.java
@@ -6,8 +6,10 @@ import lombok.Data;
 public class MemberSearch {
     private int page = 1;
     private int limit = 20;
-    private String mid;
-    private String mName;
+    private String userName; // 회원명검색
+    private String userId; // 아이디 검색
+    private String email; // 이메일 검색
+    private String nickName; // 닉네임 검색
     private String sopt; // 검색 옵션
     private String skey; // 검색 키워드
 }
