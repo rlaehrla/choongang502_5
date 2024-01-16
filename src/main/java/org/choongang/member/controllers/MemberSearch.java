3fdcede3bde9dc89cf361df4298646c3b2e5b6fd
rlaehrla <rlaehrla@naver.com>
2024-01-16 10:15:59 +0900

    회원 검색 폼 추가

--- src/main/java/org/choongang/member/controllers/MemberSearch.java
+++ src/main/java/org/choongang/member/controllers/MemberSearch.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class MemberSearch {
     private int page = 1;
     private int limit = 20;
+    private String mid;
+    private String mName;
+    private String sopt; // 검색 옵션
+    private String skey; // 검색 키워드
 }
