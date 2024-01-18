ab107b6c03a0d106c329b1970e0a30f03bd284f1
3un3un <abc1987042@naver.com>
2024-01-18 14:56:05 +0900

    마이페이지 html 이름이 너무 길어서 수정 + 컨트롤러 수정 등,,

--- src/main/java/org/choongang/member/controllers/MypageController.java
+++ src/main/java/org/choongang/member/controllers/MypageController.java
@@ -26,9 +26,8 @@ public class MypageController { //implements ExceptionProcessor {
     @GetMapping
     public String myPage(Model model) {
         commonProcess("myPage", model);
-        return utils.tpl("member/my_diary/my_page");
+        return utils.tpl("member/mypage/mypage");
     }
-
 
 
     /**
@@ -37,25 +36,14 @@ public class MypageController { //implements ExceptionProcessor {
      * @return
      */
     @GetMapping("/diary")
-    public String diary(@ModelAttribute RequestMemberInfo form, Model model, Errors errors) {
+    public String diary(@ModelAttribute RequestMemberInfo memberInfo, Model model, Errors errors) {
         commonProcess("diary", model);
-        if(memberUtil.isLogin()) {
-            AbstractMember member = memberUtil.getMember();
-            form.setNickname(member.getNickname());
-            // 프로필 메세지 불러오기
-            // 프로필 사진 불러오기
-            // 북마크 불러오기
-            // 레시피 불러오기
-            // 레시피 작성으로 이동하는 버튼 필요
-
-        }
-
-        return utils.tpl("member/my_diary/my_diary");
+        return utils.tpl("member/mypage/diary");
     }
 
-    @GetMapping("/diary/content/{num}")
-    public String content(@PathVariable("num") String num) {
-        return utils.tpl("member/my_diary/content/" + num);
+    @GetMapping("/diary/content/{tab}")
+    public String content(@PathVariable("tab") String tab) {
+        return utils.tpl("member/mypage/content/" + tab);
     }
 
 
@@ -64,10 +52,10 @@ public class MypageController { //implements ExceptionProcessor {
      * @param model
      * @return
      */
-    @GetMapping("/allorders")
-    public String allorders(Model model) {
-        commonProcess("allorders", model);
-        return utils.tpl("member/my_diary/my_allorders");
+    @GetMapping("/orders")
+    public String orders(Model model) {
+        commonProcess("orders", model);
+        return utils.tpl("member/mypage/orders");
     }
 
     /**
@@ -78,32 +66,7 @@ public class MypageController { //implements ExceptionProcessor {
     @GetMapping("/point")
     public String point(Model model) {
         commonProcess("point", model);
-        return utils.tpl("member/my_diary/my_point");
-    }
-
-    /**
-     * 회원 정보
-     * @param model
-     * @return
-     */
-/*    @GetMapping("/info")
-    public String info(@ModelAttribute RequestMemberInfo form, Model model, Errors errors) {
-        commonProcess("info", model);
-        if(memberUtil.isLogin()) {
-            AbstractMember member = memberUtil.getMember();
-            form.setUserId(member.getUserId());
-            form.setEmail(member.getEmail());
-            form.setNickname(member.getNickname());
-            form.setUsername(member.getUsername());
-            form.setProfileImage(member.getProfileImage());
-        }
-        return utils.tpl("member/my_diary/my_info");
-    }*/
-    @PostMapping("/info")
-    public String infoPs(@ModelAttribute RequestMemberInfo form, Model model, Errors errors) {
-        commonProcess("info", model);
-
-        return utils.tpl("member/my_diary/my_info");
+        return utils.tpl("member/mypage/point");
     }
 
     /**
@@ -114,43 +77,20 @@ public class MypageController { //implements ExceptionProcessor {
     @GetMapping("/recentlyview")
     public String recentlyview(Model model) {
         commonProcess("recentlyview", model);
-        return utils.tpl("member/my_diary/my_recently_view");
+        return utils.tpl("member/mypage/recently_view");
     }
 
 
 
     private void commonProcess(String mode, Model model) {
-        mode = StringUtils.hasText(mode) ? mode : "join";
-        String pageTitle = Utils.getMessage("회원가입", "commons");
-
-
-
         List<String> addCommonScript = new ArrayList<>();    // 공통 자바스크립트
         List<String> addCommonCss = new ArrayList<>();    // 공통 자바스크립트
         List<String> addCss = new ArrayList<>();
-        List<String> addScript = new ArrayList<>();    // 프론트 자바스크립트
+        List<String> addScript = new ArrayList<>();
 
         addCommonScript.add("tab");
         addCommonCss.add("tab");
 
-        if (mode.equals("login")) { // 로그인
-            pageTitle = Utils.getMessage("로그인", "commons");
-        } else if (mode.equals("join")) { // 회원가입
-            // 공통 JS
-            addCommonScript.add("address");
-            addCommonScript.add("fileManager");
-            // 회원가입 페이지를 위한 CSS, JS
-            addScript.add("member/join");
-            addScript.add("member/form");
-
-        } else if (mode.equals("find_pw")) { // 비밀번호 찾기
-            pageTitle = Utils.getMessage("비밀번호_찾기", "commons");
-        } else if (mode.equals("cart")) {
-            // 장바구니
-            pageTitle = Utils.getMessage("장바구니", "commons");
-        }
-
-        model.addAttribute("pageTitle", pageTitle);
         model.addAttribute("addCss", addCss);
         model.addAttribute("addScript", addScript);
         model.addAttribute("addCommonScript", addCommonScript);
