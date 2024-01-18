56664c813b4277a409baa1dbca3b75cc94c8e51e
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-18 15:44:56 +0900

    240118 오후 - 레시피 css, js, html 추가된 부분 - MainController, MypageController 겹치는 부분 삭제

--- src/main/resources/static/front/js/recipe/form.js
+++ src/main/resources/static/front/js/recipe/form.js
@@ -1,8 +1,63 @@
+const recipeForm = {
+    /**
+    * 입력항목 추가
+    *
+    */
+    addItem(e) {
+        const el = e.currentTarget;
+
+    },
+    /**
+    * 입력항목 제거
+    *
+    */
+    deleteItem(e) {
+
+    },
+    /**
+    * 입력항목 생성
+    *
+    */
+    createItem(name) {
+        const rows = document.createElement("div");
+        const inputText = document.createElement("input");
+        const inputEa = document.createElement("input");
+        const closeButton = document.createElement("button");
+        const buttonIcon = document.createTextNode("i");
+
+        rows.className = "item_box";
+
+        inputText.type = "text";
+        inputEa.type="text";
+
+        inputText.name = name;
+        inputEa.name = `${name}Ea`;
+
+        closeButton.type = "button";
+        buttonIcon.className = "xi-close";
+
+        closeButton.appendChild(buttonIcon);
+
+        rows.appendChild(inputText);
+        rows.appendChild(inputEa);
+        rows.appendChild(closeButton);
+
+        return rows;
+    }
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     const thumbs = document.getElementsByClassName("image1_tpl_box");
     for (const el of thumbs) {
         thumbsClickHandler(el);
     }
+
+     /* 입력 항목 추가 버튼 처리 S */
+     const buttons = document.getElementsByClassName("add_input_item");
+     for (const el of buttons) {
+        el.addEventListener("click", recipeForm.addItem);
+     }
+     /* 입력 항목 추가 버튼 처리 E */
 });
 
 /**
