724ba3d0a3129eede76f63472d0538e754b7590d
3un3un <abc1987042@naver.com>
2024-01-18 12:16:06 +0900

    레시피 수정

--- src/main/resources/static/front/js/recipe/form.js
+++ src/main/resources/static/front/js/recipe/form.js
@@ -1,114 +1,74 @@
-
-function callbackFileUpload(files) {
-     if (!files || files.length == 0) {
-            return;
-        }
-
-        const file = files[0];
-
-        let html = document.getElementById("image1_tpl").innerHTML;
-
-        const imageUrl = file.thumbsUrl.length > 0 ? file.thumbsUrl.pop() : file.fileUrl;
-        const seq = file.seq;
-
-        html = html.replace(/\[seq\]/g, seq)
-                    .replace(/\[imageUrl\]/g, imageUrl);
-
-        const domParser = new DOMParser();
-        const dom = domParser.parseFromString(html, "text/html");
-
-        const imageTplEl = dom.querySelector(".image1_tpl_box");
-
-        // 무조건 있어야 함
-        const main_image = document.getElementById("main_image");
-        // 없을 수도, 여러 개일 수도 있음
-/*        const howTo_image = document.getElementById("howTo_image");*/
-        main_image.innerHTML = "";
-/*        howTo_image.innerHTML = "";*/
-
-        main_image.appendChild(imageTplEl);
-        /*howTo_image.appendChild(imageTplEl);*/
-}
-
-/*
-* 파일 삭제 후 후속처리 함수
-*
-* @param seq : 파일 등록 번호
-*/
-
-function callbackFileDelete(seq) {
-    const fileEl = document.getElementById(`file_${seq}`);
-    fileEl.parentElement.removeChild(fileEl);
-}
-
-
-
-/*window.addEventListener("DOMContentLoaded", function() {
-    const { loadEditor } = commonLib;
-
-
-    loadEditor("html_top")
-        .then(editor => window.editor1 = editor)
-        .catch(err => console.error(err));
-
-    loadEditor("html_bottom")
-        .then(editor => window.editor2 = editor)
-        .catch(err => console.error(err));
+window.addEventListener("DOMContentLoaded", function() {
+    const thumbs = document.getElementsByClassName("image1_tpl_box");
+    for (const el of thumbs) {
+        thumbsClickHandler(el);
+    }
 });
 
-*//*
-*/
 /**
-* 파일 업로드 후 후속처리 함수
+* 파일 업로드 처리 콜백
 *
-*//*
 */
-/*
 function callbackFileUpload(files) {
-    if (!files || files.length == 0) {
-        return;
-    }
-
     const tpl = document.getElementById("image1_tpl").innerHTML;
     const domParser = new DOMParser();
-    const targetTop = document.getElementById("main_image");
-    const targetBottom = document.getElementById("howTo_image");
-
+    const targetEl = document.getElementById("main_images");
+    const thumbsEl = document.querySelector(".main_image_box .thumbs");
     for (const file of files) {
-        const editor = file.location == 'html_bottom' ? editor2 : editor1;
-        const target = file.location == 'html_bottom' ? targetBottom : targetTop;
 
-        editor.execute('insertImage', { source: file.fileUrl });
+        let html = tpl;
+        html = html.replace(/\[seq\]/g, file.seq)
+                    .replace(/\[imageUrl\]/g, file.fileUrl)
+                    .replace(/\[fileName\]/g, file.fileName);
 
-*//*
-*/
-/*        html = html.replace(/\[seq\]/g, seq)
-                            .replace(/\[imageUrl\]/g, imageUrl);*//*
-*/
-/*
+        const dom = domParser.parseFromString(html, "text/html");
+        const imageBox = dom.querySelector(".image1_tpl_box");
 
-        *//*
-*/
-/* 템플릿 데이터 치환 S *//*
-*/
-/*
-         let html = tpl;
-         html = html.replace(/\[seq\]/g, file.seq)
-                     .replace(/\[imageUrl\]/g, file.imageName)
-                     .replace(/\[imageUrl\]/g, file.imageUrl);
+        if (!targetEl.classList.contains("uploaded")) {
+            targetEl.classList.add("uploaded")
+        }
 
-         const dom = domParser.parseFromString(html, "text/html");
-         const fileBox = dom.querySelector(".image1_tpl_box");
-         target.appendChild(fileBox);
+        targetEl.style.backgroundImage=`url('${file.fileUrl}')`;
+        targetEl.style.backgroundSize='cover';
+        targetEl.style.backgroundRepeat='no-repeat';
+        targetEl.style.backgroundPosition="center center";
 
-        *//*
-*/
-/* 템플릿 데이터 치환 E *//*
-*/
-/*
+        thumbsEl.appendChild(imageBox);
+        thumbsClickHandler(imageBox);
     }
-}*//*
+}
 
 
-
+/**
+* 파일 삭제 처리 콜백
+*
 */
+function callbackFileDelete(seq) {
+    const el = document.getElementById(`file_${seq}`);
+    el.parentElement.removeChild(el);
+
+    const thumbs = document.getElementsByClassName("image1_tpl_box");
+    if (thumbs.length > 0) {
+        thumbs[0].click();
+    } else {
+        const mainImageEl = document.getElementById("main_images");
+        mainImageEl.style.backgroundImage = null;
+        mainImageEl.classList.remove("uploaded");
+    }
+
+}
+
+/**
+* 썸네일 클릭 이벤트 처리
+*
+*/
+function thumbsClickHandler(thumb) {
+
+   const mainImageEl = document.getElementById("main_images");
+
+   thumb.addEventListener("click", function() {
+    console.log("클릭!")
+    const url = this.dataset.url;
+    mainImageEl.style.backgroundImage=`url('${url}')`;
+  });
+}
