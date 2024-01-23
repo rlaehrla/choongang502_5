05843604318f05fe4932155aeefc272a94ebb63c
3un3un <abc1987042@naver.com>
2024-01-23 09:23:40 +0900

    레시피 edit 오류 수정 howto 자바스크립트 추가 howto 엔터티

--- src/main/java/org/choongang/recipe/services/RecipeSaveService.java
+++ src/main/java/org/choongang/recipe/services/RecipeSaveService.java
@@ -1,20 +1,29 @@
 package org.choongang.recipe.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.service.FileUploadService;
 import org.choongang.member.MemberUtil;
 import org.choongang.recipe.controllers.RequestRecipe;
+import org.choongang.recipe.entities.HowToCook;
 import org.choongang.recipe.entities.Recipe;
+import org.choongang.recipe.repositories.HowToCookRepository;
 import org.choongang.recipe.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeSaveService {
     private final MemberUtil memberUtil;
     private final RecipeRepository recipeRepository;
+    private final HowToCookRepository howToCookRepository;
     private final FileUploadService fileUploadService;
+    private ObjectMapper om;
 
     public Recipe save(RequestRecipe form) {
         String mode = form.getMode();
@@ -23,12 +32,18 @@ public class RecipeSaveService {
         Long seq = form.getSeq();
 
         Recipe data = null;
+        HowToCook howToCook = null;
         if(seq != null && mode.equals("edit")) { // 수정
             data = recipeRepository.findById(seq).orElseThrow(RecipeNotFoundException::new);
+            howToCook = howToCookRepository.findById(seq).orElseThrow(null);
         } else { // 작성
             data = new Recipe();
             data.setGid(form.getGid());
             data.setMember(memberUtil.getMember());
+
+            howToCook = new HowToCook();
+            howToCook.setGid(form.getGid());
+
 
         }
         data.setRcpName(form.getRcpName());
@@ -38,12 +53,51 @@ public class RecipeSaveService {
         data.setSubCategory(form.getSubCategory());
         data.setAmount(form.getAmount());
         //JSON
-        data.setRequiredIng(form.getRequiredIngJSON());
-        data.setSubIng(form.getSubIngJSON());
-        data.setCondiments(form.getCondimentsJSON());
+        ObjectMapper om = new ObjectMapper();
+
+        List<String> _requiredIng = new ArrayList<>();
+        String[] requiredIng = form.getRequiredIng();
+        String[] requiredIngEa = form.getRequiredIngEa();
+        if (requiredIng != null) {
+            for (int i = 0; i < requiredIng.length; i++) {
+                _requiredIng.add(String.format("%s_%s", requiredIng[i], requiredIngEa[i]));
+            }
+        }
+
+        List<String> _subIng = new ArrayList<>();
+        String[] subIng = form.getSubIng();
+        String[] subIngEa = form.getSubIngEa();
+        if (subIng != null) {
+            for (int i = 0; i < subIng.length; i++) {
+                _requiredIng.add(String.format("%s_%s", subIng[i], subIngEa[i]));
+            }
+        }
+
+        List<String> _condiments = new ArrayList<>();
+        String[] condiments = form.getCondiments();
+        String[] condimentsEa = form.getCondimentsEa();
+        if (subIng != null) {
+            for (int i = 0; i < condiments.length; i++) {
+                _requiredIng.add(String.format("%s_%s", condiments[i], condimentsEa[i]));
+            }
+        }
+
+        try {
+            data.setRequiredIng(om.writeValueAsString(_requiredIng));
+            data.setSubIng(om.writeValueAsString(_subIng));
+            data.setCondiments(om.writeValueAsString(_condiments));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
         // ---
 
+        howToCook.setContent(form.getContent().toString());
+        howToCook.setRecipe(data);
+
+
         recipeRepository.saveAndFlush(data);
+        howToCookRepository.saveAndFlush(howToCook);
 
         fileUploadService.processDone(data.getGid());
 
