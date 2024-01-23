48497853245a0c65ea5d4431d925ce1a62cf372d
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-23 17:51:41 +0900

    240123 오후 - 채팅 기능 작업중

--- src/main/resources/static/common/js/chat.js
+++ src/main/resources/static/common/js/chat.js
@@ -3,9 +3,16 @@ window.addEventListener("DOMContentLoaded", function() {
 
     webSocket.onopen = function(e) {
        console.log("연결 성립!", e);
+
+       webSocket.send("메세지!");
     };
 
     webSocket.onclose = function(e) {
        console.log("연결 종료!", e);
+    };
+
+    // 서버쪽에서 전송 -> 수신
+    webSocket.onmessage = function(message) {
+       console.log(message);
     };
 });
