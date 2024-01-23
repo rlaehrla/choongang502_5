e555fcb38ba51dcdff878fe94983789bab1f0ac7
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-23 17:03:42 +0900

    240123 오후 - 채팅 기능 작업중

---
+++ src/main/java/org/choongang/chatting/configs/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package org.choongang.chatting.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "chat").setAllowedOrigins("*");
+    }
+}
