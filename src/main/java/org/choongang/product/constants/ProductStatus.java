042a63034977ca63ec63d5b7885f7ca025b8e9b5
ddoonsim <2sujg97@naver.com>
2024-01-24 13:31:57 +0900

    블로그 판매 상품 목록에 상품 판매 상태 추가

--- src/main/java/org/choongang/product/constants/ProductStatus.java
+++ src/main/java/org/choongang/product/constants/ProductStatus.java
@@ -1,7 +1,16 @@
 package org.choongang.product.constants;
 
+import lombok.Getter;
+
 public enum ProductStatus {
-    SALE, // 판매중
-    OUT_OF_STOCK, // 품절
-    PREPARE, // 상품 준비중
+    SALE("판매중"), // 판매중
+    OUT_OF_STOCK("품절"), // 품절
+    PREPARE("상품 준비중") ; // 상품 준비중
+
+    @Getter
+    private final String description ;
+
+    ProductStatus(String description) {
+        this.description = description ;
+    }
 }
