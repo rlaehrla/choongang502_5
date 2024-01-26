a157e207cd3633f26b1f8a966c76abb49bd347a7
ddoonsim <2sujg97@naver.com>
2024-01-26 11:55:51 +0900

    주문완료 상태일 때만 후기 작성 버튼 활성화

--- src/main/java/org/choongang/board/service/BoardInfoService.java
+++ src/main/java/org/choongang/board/service/BoardInfoService.java
@@ -165,8 +165,10 @@ public class BoardInfoService {
         }
 
         // 리뷰 게시판 --> 특정 상품의 리뷰만 조회
-        Long productSeq = search.getProductSeq();
-        if (productSeq != null) {
+        if (request.getParameter("productSeq") != null) {
+            Long productSeq = Long.valueOf(request.getParameter("productSeq"));
+            System.out.println("==============================" + productSeq);
+            search.setProductSeq(productSeq);
             andBuilder.and(boardData.num1.eq(productSeq)) ;
         }
 
