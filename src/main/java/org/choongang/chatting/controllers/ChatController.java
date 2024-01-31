b89e30deba9e36fa11f2c0449d63891867960be8
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-31 19:36:43 +0900

    농부블로그 판매상품 css 수정 / 채팅 부분 내일 추가예정

--- src/main/java/org/choongang/chatting/controllers/ChatController.java
+++ src/main/java/org/choongang/chatting/controllers/ChatController.java
@@ -1,11 +1,15 @@
 package org.choongang.chatting.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.chatting.service.ChatHistoryInfoService;
+import org.choongang.chatting.service.ChatHistorySaveService;
+import org.choongang.chatting.service.ChatRoomInfoService;
 import org.choongang.commons.ExceptionProcessor;
 import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -17,13 +21,24 @@ import java.util.List;
 public class ChatController implements ExceptionProcessor {
 
     private final Utils utils;
-
+    private final ChatHistoryInfoService chatHistoryInfoService;
+    private final ChatHistorySaveService chatHistorySaveService;
+    private final ChatRoomInfoService chatRoomInfoService;
 
     @GetMapping
     public String index(Model model) {
         commonProcess("main", model);
 
         return utils.tpl("chat/index");
+    }
+
+    @GetMapping("/{userId}")
+    public String room(@PathVariable("userId") String userId, Model model){
+
+        //chatRoomInfoService.getList();
+
+
+        return utils.tpl("chat/room");
     }
 
     private void commonProcess(String mode, Model model) {
