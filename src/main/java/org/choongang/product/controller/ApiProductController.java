3d9c3ba296a602d18760b60b72babd1009281309
yunii118 <lyhee118@gmail.com>
2024-01-31 18:13:12 +0900

    상품 찜하기

---
+++ src/main/java/org/choongang/product/controller/ApiProductController.java
@@ -0,0 +1,29 @@
+package org.choongang.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.rests.JSONData;
+import org.choongang.product.service.ProductWishService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ApiProductController {
+
+    private final ProductWishService productWishService;
+
+    @GetMapping("/save_post/{pSeq}")
+    public JSONData<Object> savePost(@PathVariable("pSeq") Long pSeq){
+        productWishService.save(pSeq);
+
+        return new JSONData<>();
+    }
+
+    @DeleteMapping("/save_post/{pSeq}")
+    public JSONData<Object> deletePost(@PathVariable("pSeq") Long pSeq){
+        productWishService.delete(pSeq);
+
+        return new JSONData<>();
+    }
+
+}
